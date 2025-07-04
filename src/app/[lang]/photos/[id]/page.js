9260890a715eb578b0e0c11c82e9bf4eed94c4be
001Rakib <rakibrakibrakib354@gmail.com>
2025-07-04 23:11:52 +0600

    project completed

---
+++ src/app/[lang]/photos/[id]/page.js
@@ -0,0 +1,6 @@
+import PhotoDetails from "@/components/PhotoDetails";
+
+const PhotoDetailsPage = ({ params: { id, lang } }) => {
+  return <PhotoDetails id={id} lang={lang} />;
+};
+export default PhotoDetailsPage;
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import LWSLogo from "../public/lws_logo.png";
-
 const Logo = () => {
   return (
     <Link href="/">
       <Image
         className="max-w-[100px] md:max-w-[165px]"
-        src={LWSLogo}
+        src="/lws_logo.png"
         alt="Lws"
         height={100}
         width={165}
